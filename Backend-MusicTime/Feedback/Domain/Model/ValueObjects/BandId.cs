809fbbb8a481ea3aa04083211e5afc6f37c6ce5f
x5Anthony <Anthony.021217@outlook.com>
2024-06-07 20:32:27 -0500

    add: UserId and BandId Value Objects

---
+++ Backend-MusicTime/Feedback/Domain/Model/ValueObjects/BandId.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend_MusicTime.Feedback.Domain.Model.ValueObjects;
+
+public record BandId
+{
+    int bandId { get; }
+    public BandId(int bandId)
+    {
+        if (bandId < 0)
+        {
+            throw new ArgumentOutOfRangeException("User ID cannot be negative", nameof(bandId));
+        }
+        this.bandId = bandId;
+    }
+}
---
+++ Backend-MusicTime/Feedback/Domain/Model/ValueObjects/UserId.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend_MusicTime.Feedback.Domain.Model.ValueObjects;
+
+public record UserId
+{
+    int userId {  get; }
+    public UserId(int userId)
+    {
+        if (userId < 0)
+        {
+            throw new ArgumentOutOfRangeException("User ID cannot be negative", nameof(userId));
+        }
+        this.userId = userId;
+    }
+}
+
